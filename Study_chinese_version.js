441d90412ff0afa17cea7d20e5feae850cfbf240
javalan <36924867+javalan@users.noreply.github.com>
2026-03-21 10:26:03 +0800

    Increment version to 1.7 in Study_chinese_version.js

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -1,5 +1,5 @@
 // Study_chinese_version.js
 window.STUDY_CHINESE_VERSION = {
-    version: "1.6",               // increment this when you release a new version
+    version: "1.7",               // increment this when you release a new version
     install_video: "https://d1oegedfje2ody.cloudfront.net/video5_en.mp4" // optional, update if needed
 };

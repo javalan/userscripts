9e2a2b119529230043bf628a8a4a957a212d7b18
javalan <36924867+javalan@users.noreply.github.com>
2026-03-21 10:23:16 +0800

    Add files via upload

---
+++ Study_chinese_version.js
@@ -0,0 +1,5 @@
+// Study_chinese_version.js
+window.STUDY_CHINESE_VERSION = {
+    version: "1.6",               // increment this when you release a new version
+    install_video: "https://d1oegedfje2ody.cloudfront.net/video5_en.mp4" // optional, update if needed
+};

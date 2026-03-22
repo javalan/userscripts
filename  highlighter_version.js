121c9d8eb55142813a693f1dc613b3a87138629f
javalan <36924867+javalan@users.noreply.github.com>
2026-03-22 15:37:57 +0800

    Update  highlighter_version.js

---  highlighter_version.js
+++
@@ -1,4 +0,0 @@
-window.HIGHLIGHTER_VERSION = {
-    version: "1.1",
-    install_video: "https://your-video-url-here.mp4"
-};
---
+++ highlighter_version.js
@@ -0,0 +1,4 @@
+window.HIGHLIGHTER_VERSION = {
+    version: "1.1",
+    install_video: "https://d1oegedfje2ody.cloudfront.net/video5_en.mp4"
+};

8f00fa8ca160538d322e80407c09208162aea4f6
javalan <36924867+javalan@users.noreply.github.com>
2026-03-27 11:20:42 +0800

    Update Study_chinese_version.js

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -1,5 +1,5 @@
-console.log("VERSION FILE VALUE:", "2.3");
+console.log("VERSION FILE VALUE:", "2.4");
 window.STUDY_CHINESE_VERSION = {
-    version: "2.3",
+    version: "2.4",
     install_video: "..."
 };

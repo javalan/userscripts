118140174c2b1ffa3ab1f3ca820aabb64eecce35
javalan <36924867+javalan@users.noreply.github.com>
2026-03-27 11:10:29 +0800

    Update Study_chinese_version.js

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -1,5 +1,5 @@
-// Study_chinese_version.js
+console.log("VERSION FILE VALUE:", "2.3");
 window.STUDY_CHINESE_VERSION = {
-    version: "2.3",               // increment this when you release a new version
-    install_video: "https://d1oegedfje2ody.cloudfront.net/how_to_update.mp4" // optional, update if needed
+    version: "2.3",
+    install_video: "..."
 };

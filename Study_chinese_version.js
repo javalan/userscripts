6c0a68814b6f69f5fa0a5a8a5deaa7bb8f29e1eb
javalan <36924867+javalan@users.noreply.github.com>
2026-04-13 20:10:14 +0800

    Refactor changelog to support multiple languages

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -1,5 +1,10 @@
 window.STUDY_CHINESE_VERSION = {
     version: "3.0",
     install_video: "https://d1oegedfje2ody.cloudfront.net/how_to_update.mp4",
-    changelog: "Bug fixes: highlight and import issues"
+    changelog: {
+        en: "Bug fixes: highlight and import issues",
+        ko: "버그 수정: 하이라이트 및 가져오기 문제",
+        ja: "バグ修正：ハイライトとインポートの問題",
+        es: "Correcciones: problemas de resaltado e importación"
+    }
 };

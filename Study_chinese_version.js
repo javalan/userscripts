9d2fccc404417e0ed7f3020f37243c1f74433afd
javalan <36924867+javalan@users.noreply.github.com>
2026-04-13 19:43:27 +0800

    Fix syntax error in Study_chinese_version.js

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -1,5 +1,5 @@
 window.STUDY_CHINESE_VERSION = {
     version: "3.0",
-    install_video: "https://d1oegedfje2ody.cloudfront.net/how_to_update.mp4"
+    install_video: "https://d1oegedfje2ody.cloudfront.net/how_to_update.mp4",
     changelog: "Bug fixes: highlight sync, import file issue"
 };

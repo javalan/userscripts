80789e98e9598327f422d7287e0808b4a5777c35
javalan <36924867+javalan@users.noreply.github.com>
2026-05-10 19:56:55 +0800

    Update Study_chinese_version.js

--- Study_chinese_version.js
+++ Study_chinese_version.js
@@ -2,9 +2,9 @@ window.STUDY_CHINESE_VERSION = {
     version: "3.0",
     install_video: "https://d1oegedfje2ody.cloudfront.net/how_to_update.mp4",
     changelog: {
-        en: "Bug fixes: highlight and import issues",
-        ko: "버그 수정: 하이라이트 및 가져오기 문제",
-        ja: "バグ修正：ハイライトとインポートの問題",
-        es: "Correcciones: problemas de resaltado e importación"
+        en: "Bug fixes",
+        ko: "버그 수정",
+        ja: "バグ修正",
+        es: "Correcciones"
     }
 };
